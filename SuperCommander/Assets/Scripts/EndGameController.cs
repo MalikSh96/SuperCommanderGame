d58276201cf7ccad0bece25f6df1554a9db7b5cd
Malik <malik96sharfo@hotmail.com>
2019-03-19 22:26:19 +0100

    Game is ready
    Game is finally ready, still issues with the collisions counting multiple times..

---
+++ SuperCommander/Assets/Scripts/EndGameController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class EndGameController : MonoBehaviour
+{
+    public string menu;
+
+    // Update is called once per frame
+    void Update()
+    {
+        EndGame();   
+    }
+
+    public void EndGameReturnMenu()
+    {
+        SceneManager.LoadScene(menu);
+    }
+
+    public void EndGame()
+    {
+        if (Input.GetKey("escape"))
+        {
+            Application.Quit();
+        }
+    }
+}
---
+++ SuperCommander/Assets/Scripts/LoadStartGameController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class LoadStartGameController : MonoBehaviour
+{
+    public string levelToLoad;
+
+    private void Update()
+    {
+        
+    }
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene(levelToLoad); 
+    }
+}
